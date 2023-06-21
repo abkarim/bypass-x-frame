43367d45b27319c33bc898b0f76e51b74ea32fc3
Karim <akskarim17@gmail.com>
2023-06-22 03:49:18 +0600

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bypass-x-frame
-Work in progress
+
+Still some work left to make it fully dynamic
+
+## Before
+
+![before](./images/before.png)
+
+## After
+
+![after](./images/after.png)
--- bypass.js
+++ bypass.js
@@ -1,28 +1,8 @@
-const iFrames = [...document.querySelectorAll('iframe[bypass-x-frame]')];
+const iFrames = [...document.querySelectorAll("iframe[bypass-x-frame]")];
+const bypassAPILink = "http://localhost:3000/bypass/";
 
-/**
- * Get content of iframe
- * @param {string} url - target page url 
- */
-async function getIFrameContent(url) {
-    let data = await fetch("https://cors-anywhere.herokuapp.com/" + url, {
-        method: 'GET',
-    }).then(
-        response => response.text()
-    )
-    return data;
-}
+iFrames.forEach(async (frame) => {
+    let link = frame.getAttribute("bypass-x-frame");
 
-iFrames.forEach(async frame => {
-    let link = frame.getAttribute('bypass-x-frame');
-    let html = await getIFrameContent(link)
-
-    /**
-     * Modify url for files. eg: .img, .css
-     * Get all links
-     */
-  
-
-    // Set html in iframe
-    frame.srcdoc = html;
-})
+    frame.src = bypassAPILink + link;
+});
