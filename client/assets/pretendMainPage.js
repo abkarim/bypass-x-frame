801ebd162cb5c45deb6437a940cf25e8aab3c626
Karim <akskarim17@gmail.com>
2025-07-19 18:45:02 +0600

    setting page title and favicon

--- client/assets/pretendMainPage.js
+++ client/assets/pretendMainPage.js
@@ -1,8 +1,37 @@
 const iframe = document.getElementsByTagName("iframe")[0];
+const iframeURL = new URL(iframe.src);
+
+iframe.addEventListener("load", () => {
+    iframe.contentWindow.postMessage("info", "*");
+});
 
 /**
- * Get page title
+ * Get data from iframe
  */
-iframe.addEventListener("load", () => {
-    document.title = iframe.title;
+window.addEventListener("message", (event) => {
+    if (iframeURL.origin !== event.origin) return;
+
+    /**
+     * If event.data is a object
+     * and we got a name
+     */
+    if (
+        Object.prototype.toString.call(event.data) === "[object Object]" &&
+        event.data.name !== undefined
+    ) {
+        const data = event.data;
+
+        switch (data.name) {
+            case "info":
+                document.title = data.title;
+
+                /**
+                 * Set favicon
+                 */
+                if (data.favIconHTML) {
+                    document.head.innerHTML =
+                        document.head.innerHTML + data.favIconHTML;
+                }
+        }
+    }
 });
