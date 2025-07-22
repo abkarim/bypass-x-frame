03dcf3985c9e3b98076064bbc28376bcab6309ee
Karim <akskarim17@gmail.com>
2025-07-22 17:36:37 +0600

    removed duplicate var

--- server/frontend_scripts/registerServiceWorker.js
+++ server/frontend_scripts/registerServiceWorker.js
@@ -1,15 +1,8 @@
-const origin = window.location.origin;
-
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register(`${origin}/sw.js`)
+        .register(`${window.location.origin}/sw.js`)
         .then((reg) => {
             if (!navigator.serviceWorker.controller) return;
-
-            navigator.serviceWorker.controller.postMessage({
-                type: "origin",
-                origin,
-            });
         })
         .catch((err) => console.log(err));
 }
