52ee157725b22c51f9c6c046c2410a4c774c9db2
Karim <akskarim17@gmail.com>
2025-07-22 17:49:16 +0600

    setting target to _self to open links inside iframe

--- server/frontend_scripts/addProxyToLinks.js
+++ server/frontend_scripts/addProxyToLinks.js
@@ -2,6 +2,9 @@ const origin = window.location.origin;
 
 const links = document.querySelectorAll("a[href]");
 links.forEach((link) => {
+    // set target to self
+    link.target = "_self";
+
     const currentLink = link.href;
     if (currentLink.startsWith(origin)) return;
     link.href = `${origin}/${currentLink}`;
